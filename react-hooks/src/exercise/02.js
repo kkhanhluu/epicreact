5cd5749800eceb6368a575f419f1e25ccda1a592
Khanh luu <luukhanh96@gmail.com>
2021-01-12 18:27:38 +0100

    chore: custom hooks

--- react-hooks/src/exercise/02.js
+++ react-hooks/src/exercise/02.js
@@ -3,15 +3,20 @@
 
 import * as React from 'react'
 
+function useLocalStorageState(key, initialValue = '') {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) || initialValue,
+  )
+
+  React.useEffect(() => {
+    localStorage.setItem(key, state)
+  }, [key, state])
+
+  return [state, setState]
+}
+
 function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
-
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
-
+  const [name, setName] = useLocalStorageState('name', initialName)
   function handleChange(event) {
     setName(event.target.value)
   }
@@ -19,7 +24,7 @@ function Greeting({initialName = ''}) {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
