7766ad0521bb517e5eafe0203c00cec6a096bc15
Khanh luu <luukhanh96@gmail.com>
2021-02-25 11:36:15 +0100

    remove bookshelf

--- bookshelf/src/index.js
+++
@@ -1,9 +0,0 @@
-// export * from './index.final'
-
-export * from './index.exercise'
-
-// 💯 Use `@reach/dialog`
-// export * from './index.extra-1'
-
-// 💯 Create a LoginForm component
-// export * from './index.extra-2'
